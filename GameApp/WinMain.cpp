6cf8d67429690ff87f4356b7daff0959e40478ac
이정한 <127815171+jason015634@users.noreply.github.com>
2024-07-07 08:19:13 +0900

    Add files via upload

---
+++ GameApp/WinMain.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+#include "GameApp.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+#ifdef _DEBUG
+    AllocConsole();
+    FILE* pCout;
+    FILE* pCin;
+    FILE* pCerr;
+    freopen_s(&pCin, "CONIN$", "r", stdin);
+    freopen_s(&pCout, "CONOUT$", "w", stdout);
+    freopen_s(&pCerr, "CONOUT$", "w", stderr);
+#endif
+
+    GameApp gameApp(hInstance);  // 생성자에서 아이콘,윈도우 이름만 바꾼다
+    if (!gameApp.Initialize(1920, 1080))
+        return -1;
+    gameApp.Run();
+
+ /*   Engine Engine(hInstance);
+    if (!Engine.Initialize(1920, 1080))
+        return -1;
+
+    Engine.Run();*/
+
+#ifdef _DEBUG
+    FreeConsole();
+#endif
+
+    return 0;
+}
